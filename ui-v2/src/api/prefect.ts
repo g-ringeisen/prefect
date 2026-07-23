2b7894ea85b6ce5cfffb1df699eb4e45adac3617
Gautier <gautier.ringeisen@gmail.com>
2026-07-23 21:07:54 +0200

    Updated prefect.ts

--- ui-v2/src/api/prefect.ts
+++ ui-v2/src/api/prefect.ts
@@ -38,6 +38,23 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/me": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** User Information */
+        get: operations["user_information_me_get"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/flows/": {
         parameters: {
             query?: never;
@@ -12883,6 +12900,26 @@ export interface operations {
                 };
                 content: {
                     "application/json": string;
+                };
+            };
+        };
+    };
+    user_information_me_get: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": unknown;
                 };
             };
         };
